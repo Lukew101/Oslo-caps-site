29954bcf32d2acdd6f7a77939ce6bd81702a99af
Lukew101 <109954692+Lukew101@users.noreply.github.com>
2022-12-23 22:12:14 +0100

    Drop menu work

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const mainMenu = document.querySelector('.main-menu');
 const openMenu = document.querySelector('.open-menu');
 const closeMenu = document.querySelector('.close-menu');
+const openAbout = document.querySelector('.open-about');
+const openRugby = document.querySelector('.open-rugby');
+const openGetInvolved = document.querySelector('.open-get-inv');
+const getInvolvedNav = document.querySelector('.getinv-drop');
+const rugbyNav = document.querySelector('.rugby-drop');
+const aboutNav = document.querySelector('.about-drop');
 
 
 
@@ -11,8 +17,20 @@ openMenu.addEventListener('click', function(){
 
 closeMenu.addEventListener('click', function(){
     mainMenu.classList.remove('show-menu');
-})
+});
 
+openAbout.addEventListener('click', function(){
+    aboutNav.classList.toggle('show-second-menu');
+});
+
+openRugby.addEventListener('click', function(){
+    rugbyNav.classList.toggle('show-second-menu');
+});
+
+openGetInvolved.addEventListener('click', function(){
+    getInvolvedNav.classList.toggle('show-second-menu');
+    console.log(getInvolvedNav);
+});
 
 // Home Page image slider
 const images = ['/Images/team-hands-in.jpg', '/Images/oslo-pirates-together.jpg', '/Images/captain-talk.jpg', '/Images/Line-defence.jpg', '/Images/2-caps-tackle-other.jpg', '/Images/trond-caps-together-2.jpg', '/Images/caps-player-running-ball.jpg', '/Images/caps-trond-together.jpg', '/Images/caps-2014.jpg'];
