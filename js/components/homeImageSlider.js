6a13d1fe0c27c6b1e0406ad99e9eb622442b3353
Lukew101 <109954692+Lukew101@users.noreply.github.com>
2023-02-28 21:19:54 +0100

    Slider fixed.
    Work on heading drop downs.

--- js/components/homeImageSlider.js
+++ js/components/homeImageSlider.js
@@ -1,24 +1,8 @@
 export function homeImageSlider(){
     const images = ['/Images/team-hands-in.jpg', '/Images/oslo-pirates-together.jpg', '/Images/captain-talk.jpg', '/Images/Line-defence.jpg', '/Images/2-caps-tackle-other.jpg', '/Images/trond-caps-together-2.jpg', '/Images/caps-player-running-ball.jpg', '/Images/caps-trond-together.jpg', '/Images/caps-2014.jpg'];
     const homeImage = document.querySelector('.home-image');
-    const rightImgSlider = document.querySelector('.fa-chevron-right');
-    const leftImgSlider = document.querySelector('.fa-chevron-left');
     const circleContainer = document.querySelector(".circle-container");
     let index = 1;
-    
-    function pictureSlider(){
-
-        rightImgSlider.addEventListener("click", function(){
-            index = (index + 1) % images.length;
-            homeImage.src = images[index];
-        })
-
-        leftImgSlider.addEventListener("click", function(){
-            index = (index - 1 + images.length) % images.length;
-            homeImage.src = images[index];
-        })
-    }
-    pictureSlider();
 
     for(let i = 0; i < images.length; i++){
         const circle = document.createElement("div");
@@ -53,5 +37,3 @@ export function homeImageSlider(){
         highlightCircle();
     }, 8000);
 }
-
-// Arrows don't trigger circle color change. Fix this.
